a0cb09c280fcec7d19fe9c4c0495d8485ceab58a
Jonathan Nickerson <jnickersonnh@gmail.com>
2017-11-05 14:12:21 -0500

    made Main camera its seperate entity main camera is now seperate so it can be influenced

---
+++ WheresTheWolfv8/Assets/Scripts/cameraFollow_scr.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow_scr : MonoBehaviour {
+
+	private Vector2 velocity;
+	public float smoothTimeX;
+	public float smoothTimeY;
+	private float posX;
+	private float posY;
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void FixedUpdate()
+	{
+		posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+		posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+		transform.position = new Vector3(posX, posY, transform.position.z);
+	}
+
+
+}
